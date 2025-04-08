51c41ef15692cb3ca0b3a1375cd56eb4f25ca441
Hansheng Chen <chenhansheng97@126.com>
2025-04-07 19:18:28 -0700

    Code release

---
+++ lib/core/__init__.py
@@ -0,0 +1,5 @@
+from .evaluation import *
+from .optimizer import *
+from .utils import *
+from .runners import DynamicIterBasedRunnerMod
+from .ddp_wrapper import DistributedDataParallelWrapper
---
+++ lib/pipelines/gmdit_pipeline.py
@@ -0,0 +1,338 @@
+# Copyright (c) 2025 Hansheng Chen
+
+from typing import Dict, List, Optional, Tuple, Union
+
+import torch
+
+from diffusers.models import AutoencoderKL
+from diffusers.pipelines import DiTPipeline
+from diffusers.utils.torch_utils import randn_tensor
+from diffusers.pipelines.pipeline_utils import ImagePipelineOutput
+from lib.models.architecture.gmflow import _GMDiTTransformer2DModel as GMDiTTransformer2DModel
+from lib.models.architecture.gmflow import _SpectrumMLP as SpectrumMLP
+from lib.models.diffusions.schedulers import GMFlowSDEScheduler, FlowEulerODEScheduler
+from lib.models.diffusions.gmflow import probabilistic_guidance_jit
+from lib.ops.gmflow_ops.gmflow_ops import (
+    gm_to_sample, gm_to_mean, gaussian_samples_to_gm_samples,
+    iso_gaussian_mul_iso_gaussian, gm_mul_iso_gaussian, gm_to_iso_gaussian)
+
+
+class GMFlowPipelineMixin:
+
+    def denoising_gm_convert(
+            self, gm_src, x_t_tgt, x_t_src, t_tgt, t_src, eps=1e-6, prediction_type='x0'):
+        assert isinstance(gm_src, dict)
+
+        bs = x_t_src.size(0)
+        if not isinstance(t_tgt, torch.Tensor):
+            t_tgt = torch.tensor(t_tgt, device=x_t_src.device)
+        if not isinstance(t_src, torch.Tensor):
+            t_src = torch.tensor(t_src, device=x_t_src.device)
+        if t_tgt.dim() == 0:
+            t_tgt = t_tgt.expand(bs)
+        if t_src.dim() == 0:
+            t_src = t_src.expand(bs)
+        t_tgt = t_tgt.reshape(*t_tgt.size(), *((x_t_src.dim() - t_tgt.dim()) * [1]))
+        t_src = t_src.reshape(*t_src.size(), *((x_t_src.dim() - t_src.dim()) * [1]))
+
+        sigma_src = t_src / self.scheduler.config.num_train_timesteps
+        sigma_tgt = t_tgt / self.scheduler.config.num_train_timesteps
+        alpha_src = (1 - sigma_src).clamp(min=eps)
+        alpha_tgt = (1 - sigma_tgt).clamp(min=eps)
+
+        if prediction_type == 'u':
+            gm_src = self.u_to_x_0(gm_src, x_t_src, t_src)
+        else:
+            assert prediction_type == 'x0'
+
+        gaussian_mask = iso_gaussian_mul_iso_gaussian(
+            dict(
+                mean=x_t_tgt / alpha_tgt,
+                var=(sigma_tgt / alpha_tgt).square()),
+            dict(
+                mean=x_t_src / alpha_src,
+                var=(sigma_src / alpha_src).square()),
+            1, -1)
+        gm_output_tgt = gm_mul_iso_gaussian(gm_src, gaussian_mask, 1, 1)[0]
+        return gm_output_tgt
+
+    def u_to_x_0(self, denoising_output, x_t, t, eps=1e-6):
+        if isinstance(denoising_output, dict) and 'logweights' in denoising_output:
+            x_t = x_t.unsqueeze(-4)
+
+        bs = x_t.size(0)
+        if not isinstance(t, torch.Tensor):
+            t = torch.tensor(t, device=x_t.device)
+        if t.dim() == 0:
+            t = t.expand(bs)
+        t = t.reshape(*t.size(), *((x_t.dim() - t.dim()) * [1]))
+
+        sigma = t / self.scheduler.config.num_train_timesteps
+
+        if isinstance(denoising_output, dict):
+            if 'logweights' in denoising_output:
+                means_x_0 = x_t - sigma * denoising_output['means']
+                logstds_x_0 = denoising_output['logstds'] + torch.log(sigma.clamp(min=eps))
+                return dict(
+                    means=means_x_0,
+                    logstds=logstds_x_0,
+                    logweights=denoising_output['logweights'])
+            elif 'var' in denoising_output:
+                mean = x_t - sigma * denoising_output['mean']
+                var = denoising_output['var'] * sigma.square()
+                return dict(mean=mean, var=var)
+            else:
+                raise ValueError('Invalid denoising_output.')
+
+        else:  # sample mode
+            x_0 = x_t - sigma * denoising_output
+            return x_0
+
+    @staticmethod
+    def gm_sample(gm, power_spectrum=None, n_samples=1, generator=None):
+        device = gm['means'].device
+        if power_spectrum is not None:
+            power_spectrum = power_spectrum.to(dtype=torch.float32).unsqueeze(-4)
+            shape = list(gm['means'].size())
+            shape[-4] = n_samples
+            half_size = shape[-1] // 2 + 1
+            spectral_samples = torch.randn(
+                shape, dtype=torch.float32, device=device, generator=generator) * (power_spectrum / 2).exp()
+            z_1 = spectral_samples.roll((-1, -1), dims=(-2, -1)).flip((-2, -1))[..., :half_size]
+            z_0 = spectral_samples[..., :half_size]
+            z_kr = torch.complex(z_0 + z_1, z_0 - z_1) / 2
+            gaussian_samples = torch.fft.irfft2(z_kr, norm='ortho')
+            samples = gaussian_samples_to_gm_samples(gm, gaussian_samples, axis_aligned=True)
+        else:
+            samples = gm_to_sample(gm, n_samples=n_samples)
+            spectral_samples = None
+        return samples, spectral_samples
+
+    def gm_to_model_output(self, gm, output_mode, power_spectrum=None, generator=None):
+        assert output_mode in ['mean', 'sample']
+        if output_mode == 'mean':
+            output = gm_to_mean(gm)
+        else:  # sample
+            output = self.gm_sample(gm, power_spectrum, generator=generator)[0].squeeze(-4)
+        return output
+
+    def gm_2nd_order(
+            self, gm_output, gaussian_output, x_t, t, h,
+            guidance_scale=0.0, gm_cond=None, gaussian_cond=None, avg_var=None, cfg_bias=None, ca=0.005, cb=1.0,
+            gm2_correction_steps=0):
+        if self.prev_gm is not None:
+            if cfg_bias is not None:
+                gm_mean = gm_to_mean(gm_output)
+                base_gaussian = gaussian_cond
+                base_gm = gm_cond
+            else:
+                gm_mean = gaussian_output['mean']
+                base_gaussian = gaussian_output
+                base_gaussian['var'] = base_gaussian['var'].mean(dim=(-1, -2), keepdim=True)
+                avg_var = base_gaussian['var'].mean(dim=(-3, -2, -1), keepdim=True)
+                base_gm = gm_output
+
+            gm_from_prev = self.denoising_gm_convert(
+                self.prev_gm, x_t, self.prev_x_t, t, self.prev_t, prediction_type='x0')
+            mean_from_prev = gm_to_mean(gm_from_prev)
+            self.prev_gm = gm_output
+
+            # Compute rescaled 2nd-order mean difference
+            k = 0.5 * h / self.prev_h
+            prev_h_norm = self.prev_h / self.scheduler.config.num_train_timesteps
+            _guidance_scale = guidance_scale * cb
+            err_power = avg_var * (_guidance_scale * _guidance_scale + ca)
+            mean_diff = (gm_mean - mean_from_prev) * (
+                (1 - err_power / (prev_h_norm * prev_h_norm)).clamp(min=0).sqrt() * k)
+
+            bias = mean_diff
+            # Here we fuse probabilistic guidance bias and 2nd-order mean difference to perform one single
+            # update to the base GM, which avoids cumulative errors.
+            if cfg_bias is not None:
+                bias = mean_diff + cfg_bias
+            bias_power = bias.square().mean(dim=(-3, -2, -1), keepdim=True)
+            bias = bias * (avg_var / bias_power.clamp(min=1e-6)).clamp(max=1).sqrt()
+
+            gaussian_output = dict(
+                mean=base_gaussian['mean'] + bias,
+                var=base_gaussian['var'] * (1 - bias_power / avg_var.clamp(min=1e-6)).clamp(min=1e-6))
+            gm_output = gm_mul_iso_gaussian(
+                base_gm, iso_gaussian_mul_iso_gaussian(gaussian_output, base_gaussian, 1, -1),
+                1, 1)[0]
+
+            # Additional correction steps for strictly matching the 2nd order mean difference
+            if gm2_correction_steps > 0:
+                adjusted_bias = bias
+                tgt_bias = mean_diff + gm_mean - base_gaussian['mean']
+                for _ in range(gm2_correction_steps):
+                    out_bias = gm_to_mean(gm_output) - base_gaussian['mean']
+                    err = out_bias - tgt_bias
+                    adjusted_bias = adjusted_bias - err * (
+                        adjusted_bias.norm(dim=-3, keepdim=True) / out_bias.norm(dim=-3, keepdim=True).clamp(min=1e-6)
+                    ).clamp(max=1)
+                    adjusted_bias_power = adjusted_bias.square().mean(dim=(-3, -2, -1), keepdim=True)
+                    adjusted_bias = adjusted_bias * (avg_var / adjusted_bias_power.clamp(min=1e-6)).clamp(max=1).sqrt()
+                    adjusted_gaussian_output = dict(
+                        mean=base_gaussian['mean'] + adjusted_bias,
+                        var=base_gaussian['var'] * (1 - adjusted_bias_power / avg_var.clamp(min=1e-6)).clamp(min=1e-6))
+                    gm_output = gm_mul_iso_gaussian(
+                        base_gm, iso_gaussian_mul_iso_gaussian(adjusted_gaussian_output, base_gaussian, 1, -1),
+                        1, 1)[0]
+
+        else:
+            self.prev_gm = gm_output
+
+        self.prev_x_t = x_t
+        self.prev_t = t
+        self.prev_h = h
+
+        return gm_output, gaussian_output
+
+    def init_gm_cache(self):
+        self.prev_gm = None
+        self.prev_x_t = None
+        self.prev_t = None
+        self.prev_h = None
+
+
+class GMDiTPipeline(DiTPipeline, GMFlowPipelineMixin):
+
+    def __init__(
+            self,
+            transformer: GMDiTTransformer2DModel,
+            spectrum_net: SpectrumMLP,
+            vae: AutoencoderKL,
+            scheduler: GMFlowSDEScheduler | FlowEulerODEScheduler,
+            id2label: Optional[Dict[int, str]] = None):
+        super(DiTPipeline, self).__init__()
+        self.register_modules(transformer=transformer, spectrum_net=spectrum_net, vae=vae, scheduler=scheduler)
+
+        self.labels = {}
+        if id2label is not None:
+            for key, value in id2label.items():
+                for label in value.split(","):
+                    self.labels[label.lstrip().rstrip()] = int(key)
+            self.labels = dict(sorted(self.labels.items()))
+
+    @torch.inference_mode()
+    def __call__(
+        self,
+        class_labels: List[int],
+        guidance_scale: float = 0.45,
+        generator: Optional[Union[torch.Generator, List[torch.Generator]]] = None,
+        num_inference_steps: int = 32,
+        num_inference_substeps: int = 4,
+        output_mode: str = "mean",
+        order=2,
+        orthogonal_guidance: float = 1.0,
+        gm2_coefs=[0.005, 1.0],
+        gm2_correction_steps=0,
+        output_type: Optional[str] = "pil",
+        return_dict: bool = True,
+    ) -> Union[ImagePipelineOutput, Tuple]:
+        assert 0 <= guidance_scale < 1, "guidance_scale must be in [0, 1)"
+
+        batch_size = len(class_labels)
+        latent_size = self.transformer.config.sample_size
+        latent_channels = self.transformer.config.in_channels
+
+        use_guidance = guidance_scale > 0.0
+
+        x_t = randn_tensor(
+            shape=(batch_size, latent_channels, latent_size, latent_size),
+            generator=generator,
+            device=self._execution_device,
+            dtype=self.transformer.dtype,
+        )
+
+        class_labels = torch.tensor(class_labels, device=self._execution_device).reshape(-1)
+        class_null = torch.tensor([1000] * batch_size, device=self._execution_device)
+        class_labels_input = torch.cat([class_labels, class_null], 0) if use_guidance else class_labels
+
+        # set step values
+        self.scheduler.set_timesteps(num_inference_steps * num_inference_substeps, device=self._execution_device)
+
+        self.init_gm_cache()
+
+        for timestep_id in self.progress_bar(range(num_inference_steps)):
+            t = self.scheduler.timesteps[timestep_id * num_inference_substeps]
+
+            model_input = x_t
+            if use_guidance:
+                model_input = torch.cat([model_input, x_t], dim=0)
+
+            gm_output = self.transformer(
+                model_input, timestep=t.expand(model_input.size(0)), class_labels=class_labels_input)
+
+            ori_dtype = gm_output['means'].dtype
+            gm_output = {k: v.to(torch.float32) for k, v in gm_output.items()}
+            x_t = x_t.to(torch.float32)
+            model_input = model_input.to(torch.float32)
+
+            gm_output = self.u_to_x_0(gm_output, model_input, t)
+
+            # ========== Probabilistic CFG ==========
+            if use_guidance:
+                gm_cond = {k: v[:batch_size] for k, v in gm_output.items()}
+                gm_uncond = {k: v[batch_size:] for k, v in gm_output.items()}
+                uncond_mean = gm_to_mean(gm_uncond)
+                gaussian_cond = gm_to_iso_gaussian(gm_cond)[0]
+                gaussian_cond['var'] = gaussian_cond['var'].mean(dim=(-1, -2), keepdim=True)
+                gaussian_output, cfg_bias, avg_var = probabilistic_guidance_jit(
+                    gaussian_cond['mean'], gaussian_cond['var'], uncond_mean, guidance_scale,
+                    orthogonal=orthogonal_guidance)
+                gm_output = gm_mul_iso_gaussian(
+                    gm_cond, iso_gaussian_mul_iso_gaussian(gaussian_output, gaussian_cond, 1, -1),
+                    1, 1)[0]
+            else:
+                gaussian_output = gm_to_iso_gaussian(gm_output)[0]
+                gm_cond = gaussian_cond = avg_var = cfg_bias = None
+
+            # ========== 2nd order GM ==========
+            if order == 2:
+                if timestep_id < num_inference_steps - 1:
+                    h = t - self.scheduler.timesteps[(timestep_id + 1) * num_inference_substeps]
+                else:
+                    h = t
+                gm_output, gaussian_output = self.gm_2nd_order(
+                    gm_output, gaussian_output, x_t, t, h,
+                    guidance_scale, gm_cond, gaussian_cond, avg_var, cfg_bias,
+                    ca=gm2_coefs[0], cb=gm2_coefs[1], gm2_correction_steps=gm2_correction_steps)
+
+            # ========== GM SDE step or GM ODE substeps ==========
+            x_t_base = x_t
+            t_base = t
+            for substep_id in range(num_inference_substeps):
+                if substep_id == 0:
+                    if output_mode == 'sample':
+                        power_spectrum = self.spectrum_net(gaussian_output)
+                    else:
+                        power_spectrum = None
+                    model_output = self.gm_to_model_output(
+                        gm_output, output_mode, power_spectrum=power_spectrum)
+                else:
+                    assert output_mode == 'mean'
+                    t = self.scheduler.timesteps[timestep_id * num_inference_substeps + substep_id]
+                    model_output = self.gm_to_model_output(
+                        self.denoising_gm_convert(gm_output, x_t, x_t_base, t, t_base, prediction_type='x0'),
+                        output_mode)
+                x_t = self.scheduler.step(model_output, t, x_t, return_dict=False, prediction_type='x0')[0]
+
+            x_t = x_t.to(ori_dtype)
+
+        x_t = x_t / self.vae.config.scaling_factor
+        samples = self.vae.decode(x_t).sample
+
+        samples = (samples / 2 + 0.5).clamp(0, 1)
+        samples = samples.cpu().permute(0, 2, 3, 1).float().numpy()
+
+        if output_type == "pil":
+            samples = self.numpy_to_pil(samples)
+
+        # Offload all models
+        self.maybe_free_model_hooks()
+
+        if not return_dict:
+            return (samples,)
+
+        return ImagePipelineOutput(images=samples)
